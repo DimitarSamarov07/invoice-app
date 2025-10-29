747cbf853f3036f7d580b49ca2464401d7942dc7
DimitarSamarov07 <samarovdimitar@gmail.com>
2025-10-29 23:19:52 +0200

    Add route templates

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/invoices", function () {
+
+});
+
+Route::get("/invoices/{id}", function () {
+
+});
+
+Route::post("/invoices", function () {
+
+});
+
+Route::put("/invoices/{id}", function () {
+
+});
+
