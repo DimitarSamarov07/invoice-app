6509f8e6caeb58425b1087880c6d82fc54999cd1
DimitarSamarov07 <samarovdimitar@gmail.com>
2025-10-31 11:17:34 +0200

    Commit missing request

---
+++ app/Http/Requests/GetInvoicesRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetInvoicesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'search' => 'sometimes|string|max:255',
+            'status' => 'sometimes|string|in:paid,unpaid,overdue'
+        ];
+    }
+
+    public function validationData(): array|string|null
+    {
+        // Use the query parameters as the validation data
+        return $this->query();
+    }
+}
