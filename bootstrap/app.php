04ee0206164d7df3e370366a180def211dce2c5b
DimitarSamarov07 <samarovdimitar@gmail.com>
2025-10-31 14:29:57 +0200

    Make sure errors are returned as JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
         health: '/up',
     )->withMiddleware(function (Middleware $middleware) {
-    })->withExceptions(function (Exceptions $exceptions) {
+    })->withExceptions(function ($exceptions) {
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            // Exceptions should be rendered as JSON
+            if ($request->is('api/*')) {
+                return true; // Always render JSON for 'api/*' routes
+            }
+
+            return $request->expectsJson();
+        });
     })->create();
