bb25f891923f77dd77e4f8c7dc039c26562010a1
DimitarSamarov07 <samarovdimitar@gmail.com>
2025-10-31 18:56:50 +0200

    Fix production database

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    CREATE DATABASE IF NOT EXISTS invoice_api;
+    GRANT ALL PRIVILEGES ON \`invoice_api%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
